d2917d34875c528d6935fd137140296959c9a7c0
Prakyath <prakyath2210@gmail.com>
2025-09-10 15:02:17 +0530

    EDITED

--- src/main/java/com/hospital/Hospital_management/controller/AppointmentController.java
+++ src/main/java/com/hospital/Hospital_management/controller/AppointmentController.java
@@ -3,7 +3,6 @@ package com.hospital.Hospital_management.controller;
 import com.hospital.Hospital_management.dto.AppointmentRequestDto;
 import com.hospital.Hospital_management.dto.AppointmentResponseDto;
 import com.hospital.Hospital_management.dto.ResponseDto;
-import com.hospital.Hospital_management.entity.Appointment;
 import com.hospital.Hospital_management.service.AppointmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,24 +19,24 @@ public class AppointmentController {
     private final AppointmentService appointmentService;
 
     @PostMapping
-    public ResponseEntity<ResponseDto> bookAppointmnet(@RequestBody AppointmentRequestDto requestDto) {
+    public ResponseEntity<ResponseDto> bookAppointment(@RequestBody AppointmentRequestDto requestDto) {
         return new ResponseEntity<>(appointmentService.bookAppointment(requestDto), HttpStatus.CREATED);
     }
 
     @GetMapping("/details")
-    public ResponseEntity<AppointmentResponseDto> getMyAppoint(){
+    public ResponseEntity<AppointmentResponseDto> getMyAppointment(){
         return ResponseEntity.ok(appointmentService.getAppointmentDetails());
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/approve/{id}")
-    public ResponseEntity<ResponseDto> approveAppointemr(@PathVariable Long id){
+    public ResponseEntity<ResponseDto> approveAppointment(@PathVariable Long id){
         return ResponseEntity.ok(appointmentService.approveAppointment(id));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/reject/{id}")
-    public ResponseEntity<ResponseDto> rejectAppointemr(@PathVariable Long id){
+    public ResponseEntity<ResponseDto> rejectAppointment(@PathVariable Long id){
         return ResponseEntity.ok(appointmentService.rejectAppointment(id));
     }
 
